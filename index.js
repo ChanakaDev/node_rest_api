5b3ae315e7c907f0dc5dc91c2be9642bb07dbec4
ChanakaDev <mr.mbcnb@gmail.com>
2023-08-15 15:36:36 +0530

    Added a basic get request and a listner

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const app = express();
 
 // Define a port number
 const PORT = 3000;
+
+// setting up a HTTP get request
+app.get("/", (req, res) =>
+  res.send(`Node and express server is running on port ${PORT}`)
+);
+
+// Start a server that listens for incoming network requests on a specific port
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
